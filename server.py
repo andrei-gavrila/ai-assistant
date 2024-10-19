7ed1ca5cc99239ce14b736f6b59c9147ec75b213
Andrei Gavrila <andrei.gavrila@gmail.com>
2024-10-19 20:46:31 +0000

    chore: change layout

--- server.py
+++ server.py
@@ -2,21 +2,26 @@ import streamlit as st
 
 st.title("AI Assistant :monkey:")
 
-light_bedroom = st.toggle(key = "light_bedroom", label = "Bedroom Light")
-color_bedroom = st.color_picker("Bedroom Light Color", "#0000ff")
-light_kitchen = st.toggle(key = "light_kitchen", label = "Kitchen Light")
+col1, col2 = st.columns(2)
 
-if light_bedroom:
-    st.write("Bedroom Light ON")
-else:
-    st.write("Bedroom Light OFF")
+with col1:
+    light_bedroom = st.toggle(key = "light_bedroom", label = "Bedroom Light")
+    color_bedroom = st.color_picker("Bedroom Light Color", "#0000ff")
 
-st.write("The current Bedroom Light Color is", color_bedroom)
+    if light_bedroom:
+        st.write("Bedroom Light ON")
+    else:
+        st.write("Bedroom Light OFF")
 
-if light_kitchen:
-    st.write("Kitchen Light ON")
-else:
-    st.write("Kitchen Light OFF")
+    st.write("The current Bedroom Light Color is", color_bedroom)
+
+with col2:
+    light_kitchen = st.toggle(key = "light_kitchen", label = "Kitchen Light")
+
+    if light_kitchen:
+        st.write("Kitchen Light ON")
+    else:
+        st.write("Kitchen Light OFF")
 
 if 'chat_history' not in st.session_state:
     st.session_state.chat_history = [] 
